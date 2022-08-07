8ccde1b7a08e95e20772a7c2f324247d004e47c2
Ozirska <m.ozirska2000@gmail.com>
2022-08-07 13:28:51 +0200

    added markup for events and styles

--- scripts/calendar/calendar.js
+++ scripts/calendar/calendar.js
@@ -1,14 +1,42 @@
-// import { getItem } from '../common/storage.js';
-// import { generateWeekRange } from '../common/time.utils.js';
+import { getItem } from "../common/storage.js";
+import { generateWeekRange } from "../common/time.utils.js";
 // import { renderEvents } from '../events/events.js';
-// import { createNumbersArray } from '../common/createNumbersArray.js';
+import { createNumbersArray } from "../common/createNumbersArray.js";
 
 const generateDay = () => {
+  const hoursInDay = createNumbersArray(1, 24);
+  const minInHour = createNumbersArray(1, 6);
+
+  const blockMinInHour = minInHour
+    .map(
+      (min) =>
+        `<span class='calendar__time-slot-min' data-min-timeslot=${min}></span>`
+    )
+    .join("");
+
+  return hoursInDay
+    .map((hour) => `<div class='calendar__time-slot'>${blockMinInHour}</div>`)
+    .join("");
+
   // функция должна сгенерировать и вернуть разметку дня в виде строки
   // разметка состоит из 24 часовых временных слотов (.calendar__time-slot)
 };
 
 export const renderWeek = () => {
+  const weekSchedule = document.querySelector(".calendar__week");
+  const generetedDay = generateDay();
+
+  const generetedWeek = generateWeekRange(getItem("displayedWeekStart"))
+    .map(
+      (day) =>
+        `<div class='calendar__day' data-day=${new Date(
+          day
+        ).getDate()}>${generetedDay}</div>`
+    )
+    .join("");
+
+  weekSchedule.innerHTML = generetedWeek;
+
   // функция должна сгенерировать разметку недели в виде строки и вставить ее на страницу (в .calendar__week)
   // разметка недели состоит из 7 дней (.calendar__day) отображаемой недели
   // массив дней, которые нужно отобразить, считаем ф-цией generateWeekRange на основе displayedWeekStart из storage
