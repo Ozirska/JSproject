830f2526f2d218a499c3c7d7339977a1a8c1cb19
Ozirska <m.ozirska2000@gmail.com>
2022-10-26 21:57:08 +0200

    added a function that generates a side scale

--- scripts/calendar/timescale.js
+++ scripts/calendar/timescale.js
@@ -3,22 +3,16 @@ import { createNumbersArray } from "../common/createNumbersArray.js";
 const sideTimeScale = document.querySelector(".calendar__time-scale");
 
 export const renderTimescale = () => {
-  let arrOfTime = createNumbersArray(1, 12);
+  let arrOfTime = createNumbersArray(1, 24);
 
-  let firstPart = arrOfTime.map(
-    (time) => `<div class='time-slot'>
-  <span class='time-slot_time'>${`${time} AM`}</span></div>`
-  );
+  let timeScale = arrOfTime
+    .map(
+      (time) => `<div class='time-slot'>
+  <span class='time-slot_time'>${time} </span></div>`
+    )
+    .join(" ");
 
-  let secondPart = arrOfTime.map(
-    (time) => `<div class='time-slot'>
-  <span class='time-slot_time'>${`${time} PM`}</span></div>`
-  );
-
-  let fullPart = firstPart.concat(secondPart).join("");
-  console.log(fullPart);
-
-  sideTimeScale.innerHTML = fullPart;
+  sideTimeScale.innerHTML = timeScale;
 
   // ф-ция должна генерировать разметку для боковой шкалы времени (24 часа)
   // полученную разметку вставьте на страницу с помощью innerHTML в .calendar__time-scale
